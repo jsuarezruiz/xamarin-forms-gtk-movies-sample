f39518c83c5b511aca3a17146dfb57ef01c98673
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-08-25 14:40:30 +0200

    Added icons to the main menu

---
+++ src/Movies/Movies/Converters/MenuItemTypeConverter.cs
@@ -0,0 +1,38 @@
+﻿using Movies.Models;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Movies.Converters
+{
+    public class MenuItemTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var menuItemType = (MenuItemType)value;
+
+            switch (menuItemType)
+            {
+                case MenuItemType.Discover:
+                    return "Images/movies-discover.png";
+                case MenuItemType.Movies:
+                    return "Images/movies-movie.png";
+                case MenuItemType.TVShows:
+                    return "Images/movies-shows.png";
+                case MenuItemType.Upcoming:
+                    return "Images/movies-upcoming.png";
+                case MenuItemType.Favourites:
+                    return "Images/movies-favourites.png";
+                case MenuItemType.Settings:
+                    return "Images/movies-settings.png";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
